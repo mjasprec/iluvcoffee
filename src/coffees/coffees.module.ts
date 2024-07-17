23a7a3da5b208578cdfaff9cdc1c10e803344234
MJAsprec <michael.asprec@whitecloak.com>
2024-07-17 10:27:14 +0800

    manytomany relation

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoffeeEntity } from './entities/coffees.entity';
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CoffeeEntity])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,14 +1,47 @@
-import { Injectable } from '@nestjs/common';
-import { CoffeeTypes } from './entities/coffees.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
+import { Repository } from 'typeorm';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: CoffeeTypes[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['vanilla', 'chocolate'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
+
+  findAll() {
+    return this.coffeeRepository.find();
+  }
+
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne({ where: { id: +id } });
+
+    if (coffee) {
+      throw new NotFoundException(`Coffee #${id} not found.`);
+    }
+
+    return coffee;
+  }
+
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    const newCoffee = await this.coffeeRepository.create(createCoffeeDto);
+
+    return this.coffeeRepository.save(newCoffee);
+  }
+
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const coffee = await this.coffeeRepository.preload({
+      id: +id,
+      ...updateCoffeeDto,
+    });
+
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found.`);
+    }
+
+    return this.coffeeRepository.save(coffee);
+  }
 }
